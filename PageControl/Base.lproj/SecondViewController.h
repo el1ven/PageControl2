fa012767d04394005c47c17735c512393bef4ccc
el1ven <tangGPA@gmail.com>
2014-03-18 21:25:25 +0800

    first commit

---
+++ PageControl/Base.lproj/SecondViewController.h
@@ -0,0 +1,13 @@
+//
+//  SecondViewController.h
+//  PageControl
+//
+//  Created by el1ven on 14-3-18.
+//  Copyright (c) 2014年 el1ven. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SecondViewController : UIViewController
+
+@end
---
+++ PageControl/Base.lproj/SecondViewController.m
@@ -0,0 +1,38 @@
+//
+//  SecondViewController.m
+//  PageControl
+//
+//  Created by el1ven on 14-3-18.
+//  Copyright (c) 2014年 el1ven. All rights reserved.
+//
+
+#import "SecondViewController.h"
+
+@interface SecondViewController ()
+
+@end
+
+@implementation SecondViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
